91f323e558d2c5dc9248b7b9d440e781ec42a17d
hu_tianyi <tianyi.hu@duke.edu>
2023-04-08 15:28:39 -0400

    modify g2o for better compatibility

--- Thirdparty/g2o/build/CMakeFiles/g2o.dir/DependInfo.cmake
+++ Thirdparty/g2o/build/CMakeFiles/g2o.dir/DependInfo.cmake
@@ -5,13 +5,14 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/stuff/os_specific.c" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/os_specific.c.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/stuff/os_specific.c" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/os_specific.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_C
   "UNIX"
+  "g2o_EXPORTS"
   )
 
 # The include file search paths:
@@ -22,41 +23,42 @@ set(CMAKE_C_TARGET_INCLUDE_PATH
   "/usr/local/include/eigen3"
   )
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/batch_stats.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/batch_stats.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/cache.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/cache.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/estimate_propagator.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/estimate_propagator.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/factory.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/factory.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/hyper_dijkstra.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_dijkstra.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/hyper_graph.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_graph.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/hyper_graph_action.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_graph_action.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/jacobian_workspace.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/jacobian_workspace.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/marginal_covariance_cholesky.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/marginal_covariance_cholesky.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/matrix_structure.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/matrix_structure.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/optimizable_graph.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimizable_graph.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/optimization_algorithm.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/optimization_algorithm_factory.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_factory.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/optimization_algorithm_gauss_newton.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_gauss_newton.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/optimization_algorithm_levenberg.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_levenberg.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/optimization_algorithm_with_hessian.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_with_hessian.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/parameter.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/parameter.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/parameter_container.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/parameter_container.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/robust_kernel.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/robust_kernel_factory.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel_factory.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/robust_kernel_impl.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel_impl.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/solver.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/solver.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/core/sparse_optimizer.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/sparse_optimizer.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/stuff/property.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/property.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/stuff/string_tools.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/string_tools.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/stuff/timeutil.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/timeutil.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/types/types_sba.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_sba.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/types/types_seven_dof_expmap.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_seven_dof_expmap.cpp.o"
-  "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/g2o/types/types_six_dof_expmap.cpp" "/home/ying/SLAMCode/ORB_SLAM3/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_six_dof_expmap.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/batch_stats.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/batch_stats.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/cache.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/cache.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/estimate_propagator.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/estimate_propagator.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/factory.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/factory.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/hyper_dijkstra.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_dijkstra.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/hyper_graph.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_graph.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/hyper_graph_action.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/hyper_graph_action.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/jacobian_workspace.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/jacobian_workspace.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/marginal_covariance_cholesky.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/marginal_covariance_cholesky.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/matrix_structure.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/matrix_structure.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/optimizable_graph.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimizable_graph.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/optimization_algorithm.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/optimization_algorithm_factory.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_factory.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/optimization_algorithm_gauss_newton.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_gauss_newton.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/optimization_algorithm_levenberg.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_levenberg.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/optimization_algorithm_with_hessian.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/optimization_algorithm_with_hessian.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/parameter.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/parameter.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/parameter_container.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/parameter_container.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/robust_kernel.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/robust_kernel_factory.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel_factory.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/robust_kernel_impl.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/robust_kernel_impl.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/solver.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/solver.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/core/sparse_optimizer.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/core/sparse_optimizer.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/stuff/property.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/property.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/stuff/string_tools.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/string_tools.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/stuff/timeutil.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/stuff/timeutil.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/types/types_sba.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_sba.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/types/types_seven_dof_expmap.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_seven_dof_expmap.cpp.o"
+  "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/g2o/types/types_six_dof_expmap.cpp" "/home/tianyi/Projects/AdaptSLAM-master/Edge-assisted AdaptSLAM/Thirdparty/g2o/build/CMakeFiles/g2o.dir/g2o/types/types_six_dof_expmap.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_CXX
   "UNIX"
+  "g2o_EXPORTS"
   )
 
 # The include file search paths:
